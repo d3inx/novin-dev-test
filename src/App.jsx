0e5b5cdb459f5e2ae32cafd156c2964042cee1ad
d3inx <mr.reza.rm8224@gmail.com>
2023-08-08 10:06:10 +0330

    init project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-100'>
+      
+      <h1>Reactjs Test</h1>
+      
+      <div className='flex'>
+        <div className='w-1/4'>
+          Register
+        </div>
+        <div className='w-1/4'>
+          Login
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
